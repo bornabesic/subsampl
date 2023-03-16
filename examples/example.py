8a3ab95fca6a186c61cc06846a30ac03646f6984
Borna Bešić <git.o8yim@aleeas.com>
2023-03-16 23:43:50 +0100

    Update the examples

--- examples/example.py
+++ examples/example.py
@@ -11,12 +11,16 @@ y_range = 1000
 z_range = 30
 voxel_size = 0.025
 
+# Generate a random point cloud
 data = np.random.uniform((0, 0, 0), (x_range, y_range, z_range), size=(n, 3)).astype(np.float32)
 print("Points:", data.shape[0])
 
+# Get indices of subsampled points
 t_start = time.time()
 indices = pysubsampl.voxel_grid_subsample_3d(data, voxel_size)
 print("Subsampling took", f"{time.time() - t_start:.2f}", "s")
+
+# Subsample the point cloud using the indices
 t_start = time.time()
 data = data[indices, :]
 print("Indexing took", f"{time.time() - t_start:.2f}", "s")
